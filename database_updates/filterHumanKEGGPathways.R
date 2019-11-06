0e4ecbad7ac49aa98a6cd71fe40b8edd2fcbc344
Travis <travis.m.blimkie@gmail.com>
2019-11-06 09:46:26 -0800

    Added new versions of KEGG data.

--- database_updates/filterHumanKEGGPathways.R
+++
@@ -1,9 +0,0 @@
-
-# Generate list of pathways based on all the supported pathways in Pathview
-
-keggHumanPathways <- list.files(path = "pathways") %>%
-  grep(pattern = ".png$", value = TRUE) %>%
-  str_replace(".png$", "") %>%
-  str_replace("^hsa", "")
-
-save(keggHumanPathways, file = "data/k05_keggHumanPathways_r92.RData")
