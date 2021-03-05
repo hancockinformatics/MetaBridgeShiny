8970b0e70525edbad13936b911aedc772354ccc0
Travis <travis.m.blimkie@gmail.com>
2021-03-05 15:14:15 -0800

    Reformatted deferred.R script

--- deferred.R
+++ deferred.R
@@ -12,14 +12,12 @@ library(shinycssloaders)
 library(tidyverse)
 
 
-# Load example data -------------------------------------------------------
+# Load data -------------------------------------------------------
 
+# Examples
 load("data/examples_2.RData")
 
-
-# Load KEGG database files ------------------------------------------------
-
-# KEGG updated on/around November 1st, 2019
+# Load KEGG database files, updated on/around November 1st, 2019
 load("data/k00_keggCompounds_r92.RData")
 load("data/k02b_keggEnzymeShortNames_r92.RData")
 
@@ -28,40 +26,31 @@ load("data/k03_keggGenes_r92.RData")
 keggGenes <- keggGenes %>%
   dplyr::select(-KEGG) %>%
   unique()
-
 load("data/k04_keggPathways_r92.RData")
 load("data/k05_keggHumanPathways_r92.RData")
 
-
-# Load MetaCyc database files ---------------------------------------------
-
-# Updated to MetaCyc v23, on September 19th, 2019
+# Load MetaCyc database files, updated on/around September 19th, 2019
 load("data/m01_metaCycDBLinks_v23.RData")
 load("data/m02_metaCycCompoundsReactions_v23.RData")
 load("data/m03_metaCycReactionsGenes_v23.RData")
 load("data/m04_metaCycGeneIDs_v23.RData")
 load("data/m05_metaCycPathways_v23.RData")
 
-
-# Load HumanCyc cross-references ------------------------------------------
-
+# Load HumanCyc cross-references
 load("data/humanRefs.RData")
 
 
-# Utility functions -------------------------------------------------------
+# Load functions ----------------------------------------------------------
 
+# Utility functions
 source(file.path("functions", "utilityFunctions.R"), local = TRUE)$value
 
-
-# Source primary mapping functions -----------------------------------------
-
+# Primary mapping functions
 source(file.path("functions", "mapGenerally.R"), local = TRUE)$value
 source(file.path("functions", "mapPathways.R"), local = TRUE)$value
 source(file.path("functions", "visualizePathways.R"), local = TRUE)$value
 
-
-# Source app/UI functions -------------------------------------------------
-
+# App and UI functions
 source(file.path("functions", "alertFunctions.R"), local = TRUE)$value
 source(file.path("functions", "generateTables.R"), local = TRUE)$value
 
