5f76de3809bbdc04f7358c73d1afab2fc18851ff
Travis <travis.m.blimkie@gmail.com>
2022-01-19 10:19:45 -0800

    Edits to visualizePathways.R - fixed typo, used renaming function to keep pathview's files out of the main app directory.

--- functions/visualizePathways.R
+++ functions/visualizePathways.R
@@ -1,6 +1,6 @@
 #' visualizePathview
 #'
-#' @param pathway Patwhay to make the diagram of
+#' @param pathway Pathway to make the diagram of
 #' @param genes Genes to be included in the pathway map
 #' @param cpd Data being used to generate the pathway map
 #'
@@ -15,15 +15,21 @@ visualizePathview <- function(pathway, genes, cpd) {
   # Generate the PNG of desired pathway, and suppress messages from pathview()
   suppressWarnings({
     pathview(
-      gene.data = genes,
-      cpd.data = cpd,
-      pathway.id = pathway,
+      gene.data   = genes,
+      cpd.data    = cpd,
+      pathway.id  = pathway,
       gene.idtype = "SYMBOL",
-      species = "hsa",
-      kegg.dir = "pathways"
+      species     = "hsa",
+      kegg.dir    = "pathways"
     )
   })
 
-  filename <- paste0("hsa", pathway, ".pathview.png")
-  return(filename)
+  # Move the file created by pathview into a separate folder
+  filename_original <- paste0("hsa", pathway, ".pathview.png")
+  filename_new <- paste0("pathways_results/", filename_original)
+
+  file.rename(from = filename_original, to = filename_newrst)
+  message("INFO: Pathway file was renamed/moved.")
+
+  return(filename_new)
 }
