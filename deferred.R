46621b4b16d4800c476fb9f23a9a7d26cd96a53c
Travis Blimkie <travis.m.blimkie@gmail.com>
2024-02-01 09:54:04 -0800

    Cleaned up deferred.R

--- deferred.R
+++ deferred.R
@@ -1,24 +1,19 @@
 # Load packages -----------------------------------------------------------
 
 suppressPackageStartupMessages({
-  library(pathview)
+  # library(pathview)
   library(rlang)
-  library(DT)
-  library(stringr)
   library(magrittr)
 })
 
 
-# Load example data -------------------------------------------------------
+# Load data ----------------------------------------------------------
 
 load("data/example_data.RData")
 
-
-# Load data ----------------------------------------------------------
-
 # Last update was in January/February 2023
-load("data/metaCyc_data_v26.RData", verbose = TRUE)
-load("data/kegg_data_r105.RData", verbose = TRUE)
+load("data/metaCyc_data_v26.RData")
+load("data/kegg_data_r105.RData")
 
 # Keep only enzyme-gene relationships
 k03_keggGenes <- k03_keggGenes %>%
@@ -31,17 +26,4 @@ k03_keggGenes <- k03_keggGenes %>%
 purrr::walk(
   .x = list.files("functions/", full.names = TRUE),
   .f = ~source(.x)
-)
-
-
-# Set global DT options ---------------------------------------------------
-
-options(
-  DT.options = list(
-    scrollX = "100%",
-    scrollY = "30vh",
-    scrollCollapse = TRUE,
-    paging  = FALSE,
-    dom     = "tir"
-  )
 )
