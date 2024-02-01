ab53a631e02ca42e524f9a7dcf06ce66915f1a1e
Travis Blimkie <travis.m.blimkie@gmail.com>
2024-02-01 09:55:28 -0800

    Got the app working again, up to Viz tab.

--- app.R
+++ app.R
@@ -7,6 +7,8 @@ suppressPackageStartupMessages({
   library(dplyr)
 })
 
+source("deferred.R")
+
 
 # UI ----------------------------------------------------------------------
 
@@ -160,7 +162,34 @@ metabridge_ui <- page_fluid(
               choices = c(Comma = ",", Tab = "\t", Semicolon = ";"),
               selected = ","
             ),
-            uiOutput("columnPickerPanel")
+
+            br(),
+
+            strong("Select an ID Type"),
+            HTML(
+              "<p>MetaBridge supports mapping with HMDB or KEGG metabolite ",
+              "IDs. Please ensure the ID selected here matches the column ",
+              "<b><u>highlighted in blue</u></b> before clicking the <b>",
+              "Proceed</b> button."
+            ),
+            radioButtons(
+              inputId = "idType",
+              label = NULL,
+              choices = c("HMDB", "KEGG"),
+              selected = character(0)
+            ),
+
+            br(),
+
+            disabled(
+              actionButton(
+                inputId = "continueToMap",
+                class = "btn-primary",
+                icon = icon("check"),
+                label = "Proceed to Mapping",
+                width = "100%"
+              ) %>% tooltip("Continue to the mapping step")
+            )
           ),
           uiOutput("uploadedTablePanel")
         )
@@ -174,10 +203,12 @@ metabridge_ui <- page_fluid(
       title = "Map",
       value = "mapPanel",
       card(
+        min_height = "90vh",
+
         layout_sidebar(
           sidebar = sidebar(
             title = "Choose a database",
-            id = "mapPabelSidebar",
+            id = "mapPanelSidebar",
             class = "d-flex",
             width = "500px",
             open = NA,
@@ -214,7 +245,13 @@ metabridge_ui <- page_fluid(
                   id = "mapButtonTT",
                   "Map your metabolites to the selected database"
                 )
-            )
+            ),
+            uiOutput("downloadPanel"),
+            uiOutput("continueToViz")
+          ),
+          div(
+            uiOutput("mappingSummaryPanel"),
+            uiOutput("mappedMetabolitePanel")
           )
         )
       )
@@ -236,6 +273,8 @@ metabridge_ui <- page_fluid(
 
     nav_panel(
       title = "Tutorial",
+      value = "tutorialPanel",
+
       div(
         class = "container my-2",
         div(
@@ -278,6 +317,8 @@ metabridge_ui <- page_fluid(
 
     nav_panel(
       title = "About",
+      value = "aboutPanel",
+
       div(
         class = "container my-2",
         div(
@@ -456,6 +497,417 @@ metabridge_ui <- page_fluid(
   )
 )
 
-metabridge_server <- function(input, output, session) {}
+
+# Server ------------------------------------------------------------------
+
+metabridge_server <- function(input, output, session) {
+
+
+  # Initialize ------------------------------------------------------------
+
+  metaboliteObject <- reactiveVal()
+  mappedMetabolites <- reactiveVal()
+  mappingObject <- reactiveVal()
+  mappingSummary <- reactiveValues(table = NULL, dbChosen = NULL)
+  mappedMetaboliteTable <- reactiveVal()
+  databaseChosen <- reactiveVal()
+  selectedMetab <- reactiveVal()
+  idTypeChosen <- reactiveVal()
+  columnPicked <- reactiveVal()
+
+
+  # Welcome buttons -------------------------------------------------------
+
+  observeEvent(
+    input$getStarted,
+    nav_select(id = "navbarLayout", selected = "uploadPanel")
+  )
+  observeEvent(
+    input$tutorial,
+    nav_select(id = "navbarLayout", selected = "tutorialPanel")
+  )
+  observeEvent(
+    input$about,
+    nav_select(id = "navbarLayout", selected = "aboutPanel")
+  )
+
+
+  # Example data ----------------------------------------------------------
+
+  observeEvent(input$tryExamples, {
+    metaboliteObject(example_data)
+    mappingObject(NULL)
+    mappedMetabolites(NULL)
+    mappingObject(NULL)
+    mappingSummary$table <- NULL
+    mappingSummary$dbChosen <- NULL
+    mappedMetaboliteTable(NULL)
+    databaseChosen(NULL)
+  })
+
+
+  # User data -------------------------------------------------------------
+
+  observeEvent({
+    input$metaboliteUpload
+    input$sep
+    input$header
+  }, {
+    if (!is.null(input$metaboliteUpload)) {
+      readr::read_delim(
+        file = input$metaboliteUpload$datapath,
+        col_names = input$header,
+        delim = input$sep
+      ) %>% metaboliteObject()
+
+      mappingObject(NULL)
+      mappedMetabolites(NULL)
+      mappingObject(NULL)
+      mappingSummary$table <- NULL
+      mappingSummary$dbChosen <- NULL
+      mappedMetaboliteTable(NULL)
+      databaseChosen(NULL)
+    }
+  })
+
+
+  # Success panel ---------------------------------------------------------
+
+  output$uploadSuccess <- renderUI({
+    input$tryExamples
+    if (is.null(metaboliteObject())) {
+      return(NULL)
+    }
+    tagList(
+      h4("Input data preview & ID selection"),
+      HTML(
+        "<p>If your data has loaded correctly, click a column to <b><u>",
+        "highlight it in blue</u></b>, select the matching ID type in the ",
+        "lower left box, then continue via the <b>Proceed</b> button.</p>"
+      )
+    )
+  })
+
+
+  # Input table -----------------------------------------------------------
+
+  output$uploadedDataTable <- DT::renderDataTable(
+    {
+      input$tryExamples
+      input$sep
+      input$header
+      input$metaboliteUpload
+      if (is.null(metaboliteObject())) {
+        return(NULL)
+      } else {
+        metaboliteObject()
+      }
+    },
+    rownames = FALSE,
+    selection = list(mode = "single", target = "column"),
+    style = "bootstrap",
+    options = list(
+      scrollX = "100%",
+      scrollY = "60vh",
+      scrollCollapse = TRUE,
+      paging  = FALSE,
+      dom = "tir"
+    )
+  )
+
+
+  # Render success panel and input table ----------------------------------
+
+  output$uploadedTablePanel <- renderUI(div(
+    uiOutput("uploadSuccess"),
+    DT::dataTableOutput("uploadedDataTable"),
+  ))
+
+
+  # Column selection ------------------------------------------------------
+
+  observeEvent({
+    input$uploadedDataTable_columns_selected
+    input$sep
+    input$header
+  }, {
+    columnIndex <- input$uploadedDataTable_columns_selected + 1
+    columnName <- colnames(metaboliteObject())[columnIndex]
+    columnPicked(columnName)
+  })
+
+  observeEvent({
+    columnPicked()
+    input$idType
+  }, {
+    if (all(length(columnPicked()) != 0 & !is.null(input$idType))) {
+      enable("continueToMap")
+    } else {
+      disable("continueToMap")
+    }
+  })
+
+
+  # Mapping ---------------------------------------------------------------
+
+  observeEvent(input$continueToMap, {
+    nav_select(id = "navbarLayout", selected = "mapPanel")
+    enable("mapButton")
+  })
+
+  observeEvent(input$mapButton, idTypeChosen(input$idType))
+
+  observeEvent(input$mapButton, {
+    databaseChosen(input$dbChosen)
+    removeUI(selector = "#mappingAlert")
+
+    mappingOutput <- mapGenerally(
+      importDF = metaboliteObject(),
+      col = columnPicked(),
+      db = databaseChosen(),
+      idType = idTypeChosen()
+    )
+
+    mappedMetabolites(mappingOutput$data)
+    mappingObject(mappingOutput)
+
+    mappingAlert(
+      status = mappingOutput$status,
+      message = mappingOutput$message,
+      suggest = mappingOutput$suggest
+    )
+  })
+
+  observeEvent(
+    input$remap,
+    nav_select(id = "navbarLayout", selected = "uploadPanel")
+  )
+
+
+  # Grouped table ---------------------------------------------------------
+
+  observeEvent(input$mapButton, {
+    results <- generateSummaryTable(
+      mappingObject(),
+      idTypeChosen(),
+      databaseChosen()
+    )
+    mappingSummary$table <- results$table
+    mappingSummary$dbChosen <- results$dbChosen
+  })
+
+  output$mappingSummaryTable <- DT::renderDataTable(
+    {
+      mappingSummary$table %>% hyperlinkTable(databaseChosen())
+    },
+    rownames = FALSE,
+    style = "bootstrap",
+    escape = FALSE,
+    selection = "single",
+    options = list(
+      scrollX = "100%",
+      scrollY = "35vh",
+      scrollCollapse = TRUE,
+      paging  = FALSE,
+      dom = "tir"
+    )
+  )
+
+  output$mappingSummaryPanel <- renderUI({
+    mappingSummary$table
+
+    if (is.null(mappingObject())) {
+      return(NULL)
+    } else if (mappingObject()$status == "error" | mappingObject()$status == "empty") {
+      return(NULL)
+    } else {
+      return(
+        tagList(
+          h3(paste0("Mapping summary: ", databaseChosen())),
+          DT::dataTableOutput("mappingSummaryTable")
+        )
+      )
+    }
+  })
+
+  observeEvent(
+    input$mappingSummaryTable_rows_selected,
+    selectedMetab(input$mappingSummaryTable_rows_selected)
+  )
+
+  observeEvent(input$mapButton, selectedMetab(NULL))
+
+
+  # Single table ----------------------------------------------------------
+
+  observeEvent({
+    selectedMetab()
+    input$mapButton
+  }, {
+    if (mappingObject()$status == "error" | mappingObject()$status == "empty") {
+      mappingObject()$data %>% mappedMetaboliteTable()
+
+    } else if (databaseChosen() == "KEGG") {
+      if (mappingSummary$dbChosen != "KEGG") {
+        return(NULL)
+      } else {
+        generateKEGGMetabTable(
+          mappingObject(),
+          mappingSummary$table,
+          selectedMetab(),
+          idTypeChosen()
+        ) %>% mappedMetaboliteTable()
+      }
+
+    } else if (databaseChosen() == "MetaCyc") {
+      if (mappingSummary$dbChosen != "MetaCyc") {
+        return(NULL)
+      } else {
+        generateMetaCycMetabTable(
+          mappingObject(),
+          mappingSummary$table,
+          selectedMetab(),
+          idTypeChosen()
+        ) %>% mappedMetaboliteTable()
+      }
+    }
+  })
+
+  output$mappedMetaboliteTable <- DT::renderDataTable(
+    {
+      if (is.null(mappingObject()) | is.null(selectedMetab())) {
+        return(data.frame())
+
+      } else if (mappingObject()$status == "success") {
+        mappedMetaboliteTable() %>% hyperlinkTable(databaseChosen())
+      }
+    },
+    rownames = FALSE,
+    style = "bootstrap",
+    escape = FALSE,
+    selection = "single",
+    options = list(
+      scrollX = "100%",
+      scrollY = "35vh",
+      scrollCollapse = TRUE,
+      paging  = FALSE,
+      dom = "tir"
+    )
+  )
+
+  output$mappedMetabolitePanel <- renderUI({
+    div(
+      if (is.null(mappingObject())) {
+        return(NULL)
+      } else if (
+        mappingObject()$status == "error" | mappingObject()$status == "empty"
+      ) {
+        tags$h3("Intermediate Results")
+      } else {
+        tagList(
+          tags$hr(),
+          tags$h3("Per-metabolite results")
+        )
+      },
+      DT::dataTableOutput("mappedMetaboliteTable")
+    )
+  })
+
+
+  # Results download ------------------------------------------------------
+
+  output$downloadPanel <- renderUI({
+    if (!is.null(mappedMetabolites())) {
+      tagList(
+        hr(),
+        strong("Download your results"),
+        p(
+          "Use the button below to download your full mapping results as ",
+          "a tab-delimited text file."
+        ),
+        downloadButton(
+          outputId = "downloadMappingData",
+          label = "Download results",
+          class = "btn-success"
+        )
+      )
+    }
+  })
+
+  cleanMappedMetabolites <- reactive({
+    req(mappedMetabolites())
+
+    if (databaseChosen() == "MetaCyc") {
+      mappedMetabolites() %>% cleanReactions(.)
+    } else {
+      mappedMetabolites()
+    }
+  })
+
+  # Export results to a file named: "originalfilename_mapped_dbChosen.tsv"
+  output$downloadMappingData <- downloadHandler(
+    filename = function() {
+      paste0(
+        ifelse(
+          test = is.null(input$metaboliteUpload),
+          yes = "example_dataset",
+          no = tools::file_path_sans_ext(input$metaboliteUpload$name)
+        ),
+        "_mapped_",
+        databaseChosen(),
+        ".tsv"
+      )
+    },
+    content = function(filename) {
+      readr::write_tsv(
+        x = cleanMappedMetabolites(),
+        file = filename
+      )
+    }
+  )
+
+
+  # Continue to Viz -------------------------------------------------------
+
+  output$continueToViz <- renderUI({
+    if (!is.null(mappedMetabolites())) {
+      if (is.null(databaseChosen()) | databaseChosen() == "MetaCyc") {
+        return(NULL)
+      } else {
+        tagList(
+          hr(),
+          strong("Visualize your results"),
+          HTML(
+            "<p>If you chose <b>KEGG</b> as the database to map your metabolites, ",
+            "you can visualize your results with ",
+            "<a href='https://bioconductor.org/packages/pathview/'>Pathview.</a>",
+            "Select a metabolite from the top table, then click the button ",
+            "below to see the pathways it's involved in.</p>"
+          ),
+          if (databaseChosen() == "KEGG" & !is.null(selectedMetab())) {
+            actionButton(
+              inputId = "visualizeButton",
+              class = "btn-primary",
+              icon = icon("eye"),
+              label = "Visualize"
+            )
+          } else {
+            disabled(actionButton(
+              inputId = "visualizeButton",
+              class = "btn-primary",
+              icon = icon("eye"),
+              label = "Visualize"
+            ) %>% tooltip("Select a metabolite from the summary table"))
+          }
+        )
+      }
+    }
+  })
+
+  observeEvent(
+    input$visualizeButton,
+    nav_select(id = "navbarLayout", selected = "vizPanel")
+  )
+}
 
 shinyApp(metabridge_ui, metabridge_server)
