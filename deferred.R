09e0f0518ec4c78a9a35e65fe52fc2a9da43645b
Travis Blimkie <travis.m.blimkie@gmail.com>
2024-02-12 09:05:18 -0800

    Added new version of KEGG data for use.

--- deferred.R
+++ deferred.R
@@ -13,7 +13,7 @@ load("example_data/example_data.RData")
 
 # Last update was in February 2024
 load("data/metaCyc_data_v27.RData")
-load("data/kegg_data_r105.RData")
+load("data/kegg_data_r109.RData")
 
 # Keep only enzyme-gene relationships
 k03_keggGenes <- k03_keggGenes %>%
